9dfac7abaaaa530fe76b0e57eea4f07a63fb4ed5
juanSe756 <juansebastiangilborda@gmail.com>
2024-01-04 22:08:05 -0500

    first spring  commit

---
+++ src/main/java/com/springProyect/SpringProyectApplication.java
@@ -0,0 +1,13 @@
+package com.springProyect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProyectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringProyectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springProyect/SpringProyectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springProyect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProyectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
